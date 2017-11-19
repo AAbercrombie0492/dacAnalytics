0fc41080181384d2e6a826727a34d87a4afd011e
Anthony Abercrombie <amarinusha@gmail.com>
2017-11-18 16:50:05 -0800

    Add dashboards

---
+++ dashboard/simple_app.py
@@ -0,0 +1,112 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+app = dash.Dash()
+
+# df = pd.read_csv(
+#     'https://gist.githubusercontent.com/chriddyp/'
+#     'cb5392c35661370d95f300086accea51/raw/'
+#     '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
+#     'indicators.csv')
+df = gpd.read_file('../data/processed/census_dac_ces_sunroof_metrics.geojson')
+available_indicators = df.columns.unique()
+
+
+# available_indicators = df['Indicator Name'].unique()
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='ces_3.0_score'
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='kwH_consumer/census tract'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    # dcc.Slider(
+    #     id='year--slider',
+    #     min=df['Year'].min(),
+    #     max=df['Year'].max(),
+    #     value=df['Year'].max(),
+    #     step=None,
+    #     marks={str(year): str(year) for year in df['Year'].unique()}
+    # )
+])
+
+@app.callback(
+    dash.dependencies.Output('indicator-graphic', 'figure'),
+    [dash.dependencies.Input('xaxis-column', 'value'),
+     dash.dependencies.Input('yaxis-column', 'value'),
+     dash.dependencies.Input('xaxis-type', 'value'),
+     dash.dependencies.Input('yaxis-type', 'value'),
+     # dash.dependencies.Input('year--slider', 'value')
+     ])
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type):
+    # dff = df[df['Year'] == year_value]
+    dff = df
+
+    return {
+        'data': [go.Scatter(
+            # x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            x = dff[xaxis_column_name],
+            # y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            y = dff[yaxis_column_name],
+            # text=dff[dff['Indicator Name'] == yaxis_column_name]['census_tract'],
+            text = dff['census_tract'],
+
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear' if xaxis_type == 'Linear' else 'log'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear' if yaxis_type == 'Linear' else 'log'
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    }
+
+if __name__ == '__main__':
+    app.run_server()
